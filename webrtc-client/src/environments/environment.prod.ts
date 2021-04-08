961248f362c663e406335925ffdcfafeff1ed4f5
Himanshu Mittal <himittal09@gmail.com>
2021-04-08 19:36:35 +0530

    deploy attemp #unknown

--- webrtc-client/src/environments/environment.prod.ts
+++ webrtc-client/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   mapsAPIKey: 'AIzaSyCsuH7ZXJAY7AD2DpQXbFNxi3aRoB8v-q0',
-  serverURL: 'https://calm-stream-20312.herokuapp.com/'
+  serverURL: 'https://calm-stream-20312.herokuapp.com'
 };
